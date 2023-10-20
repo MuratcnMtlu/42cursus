bf57675acaad3f5c8fcc5ae1aa1052e40c39dc10
MuratcnMtlu <muratcan.mutlu43@gmail.com>
2023-10-20 10:50:39 +0300

    strlcat bitti , return değerleri tam oturmadı

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+size_t ft_strlen(const char *arg) {
+    size_t i = 0;
+    while (arg[i] != '\0')
+        i++;
+    return i;
+}
+
+size_t ft_strlcat(char *dest, const char *src, size_t destsize) {
+    size_t size_Arr = ft_strlen((const char *)dest);
+    size_t i = 0;
+
+    if (destsize <= size_Arr)
+        return destsize + ft_strlen(src);
+    
+    while (src[i] != '\0' && (size_Arr + i) < (destsize - 1)) {
+        dest[size_Arr + i] = src[i];
+        i++;
+    }
+    dest[size_Arr + i] = '\0';
+    return size_Arr + ft_strlen((const char*)src);
+}
+
+// return değerleri tam o   turmadı
