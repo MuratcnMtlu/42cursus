ed179e34d9923496b904d1b9514c34f75604a678
MuratcnMtlu <muratcan.mutlu43@gmail.com>
2023-10-27 14:24:03 +0300

    denemne

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -1,12 +1,25 @@
 #include "libft.h"
+#include <string.h>
+
+char *ft_cpyStr(char * dest , const char * src, size_t size)
+{
+    int i = 0;
+    while (!size)
+    {
+        *(dest + i) = *(src + i);
+        i++;
+        size--;
+    }
+    *(dest + i) = '\0';
+    return dest;
+}
 
 char *ft_strdup(const char *s)
 {
     char *dpc;
-
     dpc = (char *)malloc(ft_strlen(s) + 1);
     if (!dpc)
         return NULL;
-    ft_strlcpy(dpc, s, ft_strlen(s) + 1);
-    return (dpc);
+    return  (ft_cpyStr(dpc, s, ft_strlen(s) + 1 ));
 }
+
---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, char const *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(src + i))
+		i++;
+	if (!n)
+		return (i);
+	while (--n && *src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (i);
+}
