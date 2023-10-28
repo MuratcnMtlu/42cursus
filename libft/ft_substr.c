b2e8abef3c82a1a48323ae2bf345eb0636fdc8ad
MuratcnMtlu <101725180+MuratcnMtlu@users.noreply.github.com>
2023-10-28 11:42:30 +0300

    substr bitti

---
+++ libft/ft_substr.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    size_t i = -1;
+    size_t ssize = ft_strlen(s);
+    char *new_Str;
+
+    if (!s)
+        return (0);
+    if (len >   ssize -(size_t)start)
+        len =   ssize - (size_t)start;
+    if (start >= ssize)
+        len = 0;
+    new_Str = (char *)malloc(len + 1);
+    if (!new_Str)
+		return (0);
+   while (++i < len)
+        new_Str[i] = s[start + i];
+    new_Str[len] = '\0';
+    return new_Str;
